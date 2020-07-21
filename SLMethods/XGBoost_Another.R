22509b8431f1c13ac1be3aecf1e8c14cc4c2d49c
Jack Elkes <Jak7774@users.noreply.github.com>
2020-07-21 08:34:52 +0100

    Examples of Methods used by the SuperLearner

---
+++ SLMethods/XGBoost_Another.R
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------
+#  Imperial Clinical Trials Unit, Imperial College London, UK
+#  Program name:     XGBoost Second Example
+#  Project:          SuperLearner
+#  Written by:       Jack Elkes 
+#  Date of creation: 20JUL2020
+#  Description:      Worked example to understand how XGBoost works
+#-----------------------------------------------------------
+
+
+library(xgboost) # for xgboost
+library(tidyverse) # general utility functions
